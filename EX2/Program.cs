9ddbd33a2e246254019663551d9b57730b811cf0
vlasov2198 <vlasov2198@gmail.com>
2023-01-10 09:50:32 +0300

    dwa

---
+++ EX2/Program.cs
@@ -0,0 +1,39 @@
+﻿    const int N = 4;
+
+    int i = 0, j = 0, k = N - 1;
+    int tmp;
+
+    int[,] array = new int[N, N]
+    {
+                {2, 3, 6, 7},
+                {3, 3, 12, 9},
+                {6, 6, 34, 12},
+                {2, 2, 3, 7},
+    };
+
+    for (int z = 0; z < N; z++)
+    {
+        for (int l = 0; l < N; l++)
+        {
+            Console.Write("{0} ", array[z, l]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+    while (i < 4)
+    {
+        tmp = array[i, j];
+        array[i, j] = array[i, k];
+        array[i, k] = tmp;
+        i++; j++; k--;
+    }
+
+    for (int z = 0; z < N; z++)
+    {
+        for (int l = 0; l < N; l++)
+        {
+            Console.Write("{0} ", array[z, l]);
+        }
+        Console.WriteLine();
+    }
